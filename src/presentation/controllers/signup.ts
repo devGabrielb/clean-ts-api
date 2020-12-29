758559caaddd612b282629dd73b9604205a166ac
devGabrielb <dev.gabrielb1@gmail.com>
2020-12-29 16:32:28 -0300

    refactor: simplfy code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,11 +3,11 @@ import { badRequest } from '../helpers/http-helper';
 import { HttpResponse, HttpResquest } from '../protocols/http';
 export default class SignUpController {
   handle(httpRequest: HttpResquest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamsError('name'));
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamsError('email'));
+    const requiredFields = ['name', 'email'];
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamsError(field));
+      }
     }
   }
 }
